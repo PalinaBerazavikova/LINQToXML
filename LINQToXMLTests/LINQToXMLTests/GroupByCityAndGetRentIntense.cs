96474803eaffb425e43721e6ba6603f184270017
PalinaBerazavikova <Palina_Berazavikova@epam.com>
2016-10-03 19:52:30 +0300

    123

---
+++ LINQToXMLTests/LINQToXMLTests/GroupByCityAndGetRentIntense.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace LINQToXMLTests
+{
+    [TestClass]
+    public class GroupByCityAndGetRentIntense
+    {
+        [TestMethod]
+        public void Task7()
+        {
+            string FileName = $@"{Environment.CurrentDirectory}\Customers.xml";
+            XDocument Document = XDocument.Load(FileName);
+            var query = Document.Elements().
+       Elements().Where(x => x.Elements("orders").Elements().Count() != 0).Select(x => new
+       {
+           city = x.Element("city").Value,
+           countOfOrders = x.Elements("orders").Elements().Count(),
+           sumRent = x.Elements("orders").Elements().Elements("total").Select(y => double.Parse(y.Value.Replace(".", ","))).Average(),
+       }).GroupBy(x => x.city).Select(x => new
+       {
+           city = x.Key,
+           averageSumRent = x.Select(y => y.sumRent).Average(),
+           averageCountOfOrders = x.Select(y => y.countOfOrders).Average(),
+       }).GroupBy(x => x);
+            
+            using (StreamWriter file = new StreamWriter($@"{Environment.CurrentDirectory}\CustomersGroupedByCityAndGetRentIntens.txt"))
+            {
+                foreach (var varGroup in query)
+                {
+                    foreach (var a in varGroup)
+                    {
+                        file.WriteLine(a.city);
+                        file.WriteLine($"{a.averageSumRent:#0.0#} ");
+                        file.WriteLine($"{ a.averageCountOfOrders:#0.0#} ");
+                    }
+                    
+                }
+            }
+        }
+    }
+}
--- LINQToXMLTests/LINQToXMLTests/TestSomethingWrong.cs
+++ LINQToXMLTests/LINQToXMLTests/TestSomethingWrong.cs
@@ -3,6 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Xml.Linq;
 using System.Linq;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace LINQToXMLTests
 {
@@ -19,6 +20,63 @@ namespace LINQToXMLTests
         {
             PostalcodeWithLetters();
         }
+        [TestMethod]
+        public void TestPhoneWithoutOperator()
+        {
+            PhoneWithoutOperator();
+        }
+        [TestMethod]
+        public void TestCommonWrong()
+        {
+            CommonWrong();
+        }
+        public void CommonWrong()
+        {
+            string FileName = $@"{Environment.CurrentDirectory}\Customers.xml";
+            XDocument Document = XDocument.Load(FileName);
+            var a = Document.Elements().
+                Elements().
+                Select(x => new
+                {
+                    name = x.Element("name").Value,
+                    phone = x.Element("phone").Value,
+                    postalcode = x.Element("postalcode"),
+                    region = x.Element("region"),
+                }).ToList();
+            var result = a.Where(x => ((x.region == null)||!(Regex.IsMatch(x.phone, @"^[(]")) || ((x.postalcode != null) && (Regex.IsMatch(x.postalcode.Value, "[^0-9-]"))))).ToList();
+            using (StreamWriter file = new StreamWriter($@"{Environment.CurrentDirectory}\CustomersCommonWrong.txt"))
+            {
+                foreach (var b in result)
+
+                {
+                    file.WriteLine($"{b.name} Phone: {b.phone}");
+                    if(b.postalcode != null) file.WriteLine($"Postalcode: {b.postalcode.Value}");
+                }
+            }
+        }
+        public void PhoneWithoutOperator()
+        {
+            string FileName = $@"{Environment.CurrentDirectory}\Customers.xml";
+            XDocument Document = XDocument.Load(FileName);
+            var a = Document.Elements().
+                Elements().
+                Select(x => new
+                {
+                    name = x.Element("name").Value,
+                    phone = x.Element("phone").Value,
+                }).ToList();
+
+            var result = a.Where(x => !(Regex.IsMatch(x.phone, @"^[(]"))).ToList();
+            using (StreamWriter file = new StreamWriter($@"{Environment.CurrentDirectory}\CustomersPhoneWithoutOperator.txt"))
+            {
+                foreach (var b in result)
+
+                {
+
+                    file.WriteLine($"{b.name} { b.phone}");
+                }
+            }
+        }
         public void PostalcodeWithLetters()
         {
             string FileName = $@"{Environment.CurrentDirectory}\Customers.xml";
@@ -27,23 +85,22 @@ namespace LINQToXMLTests
                 Elements().
                 Select(x => new
                 {
-                    region = x.Element("region"),
+                    name = x.Element("name").Value,
                     postalcode = x.Element("postalcode"),
-                    phone = x.Element("phone").Value,
                 }).ToList();
-            int res;
 
-            var result = a.Where(x =>  ((x.postalcode != null) && (Regex.IsMatch(x.postalcode.Value, "[^0-9-]"))) )))).ToList();
-            // using (StreamWriter file = new StreamWriter($@"{Environment.CurrentDirectory}\CustomersFirstDate.txt"))
-            // {
-            foreach (var b in result)
-
+            var result = a.Where(x => ((x.postalcode != null) && (Regex.IsMatch(x.postalcode.Value, "[^0-9-]")))).ToList();
+            using (StreamWriter file = new StreamWriter($@"{Environment.CurrentDirectory}\CustomersPostalcodeWithLetters.txt"))
             {
-                Console.WriteLine($"{b}");
-                //file.WriteLine($"{b.name} { b.date:MM:yyyy}");
+                foreach (var b in result)
+
+                {
+
+                    file.WriteLine($"{b.name} { b.postalcode.Value}");
+                }
             }
-            // }
         }
+
         public void NoRegion()
         {
             string FileName = $@"{Environment.CurrentDirectory}\Customers.xml";
@@ -54,8 +111,7 @@ namespace LINQToXMLTests
                 {
                     name = x.Element("name").Value,
                     region = x.Element("region"),
-                    postalcode = x.Element("postalcode"),
-                    phone = x.Element("phone").Value,
+
                 }).ToList();
 
             var result = a.Where(x => (x.region == null)).ToList();
@@ -70,3 +126,4 @@ namespace LINQToXMLTests
         }
     }
 }
+
